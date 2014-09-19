f1ce580e87928fc3ee3954e6793158557a2db78f
sbaer <steve@mcneel.com>
2014-09-19 14:16:31 -0700

    cleaned up to just basic functionality

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import os
 from google.appengine.ext import webapp
-from google.appengine.api.urlfetch import fetch
-from google.appengine.ext.webapp import template
 import mistune
-from google.appengine.api import users
+
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        """
-        user = users.get_current_user()
-
-        allowed_ids = {'112266133891816514376':('Localhost Steve', 'sbaer'),
-                       '104684131424750918512':('Steve Baer', 'sbaer'),
-                       '115053547347074595578':('Brian Gillespie', 'golsby')}
-        if not user or not allowed_ids.has_key(user.user_id()):
-            self.response.out.write("Not Authorized - " + user.user_id() )
-            return
-        """
         if "favicon" in self.request.path:
             return
         markdown_file = self.request.path[1:] + ".md"
@@ -25,7 +13,6 @@ class MainHandler(webapp.RequestHandler):
         f = open(file_path, 'r')
         markdown = f.read()
         f.close()
-        #self.response.out.write(template.render(path, template_values))
 
         html = mistune.markdown(markdown)
         self.response.out.write(html)
